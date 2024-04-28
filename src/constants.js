e51f4f8197cdec2ae5b093b6a756f3fced68e5ea
RITWIK MISHRA <ritwikmishrastudent@gmail.com>
2024-04-28 18:11:53 +0530

    portfolio

---
+++ src/constants.js
@@ -0,0 +1,17 @@
+export const scaleFactor = 4;
+
+export const dialogueData = {
+  pc: `This is my PC. I work mostly in Python these days but i am also familiar with other languages.
+    I've made a some projects as well to enhance my learning. Anyway regardless of the language, I just like programming.
+    Here is my <a href="https://github.com/Ritwik740" target="_blank">Github</a>!`,
+  "LinkedIN": `To check out some of my achievements, you can also visit my <a href="https://www.linkedin.com/in/ritwik-mishra-99997022a/" target="_blank">LinkedIN profile</a>!`,
+  "cs-degree": `As currently i am pursuing B. Tech CSE, i will upload my degree as soon as i graduate..`,
+  "sofa-table": `That's my sofa. I like to relax here and solve problems on online platforms like Coding Ninjas and Leetcode. 
+  You can check it out here: <a href="https://www.naukri.com/code360/profile/1f9f82f1-429b-42a4-ab03-dad88b8f7665" target="_blank">Coding Ninjas</a>! <a href="https://leetcode.com/u/ritwikmishrastudent/" target="_blank">Leetcode</a>! `,
+  tv: `That's my TV. I've been using it as external display for quite a long time to ease my learning process. Contact me at mishra.ritwik.03@gmail.com if you have any interesting opportunities!`,
+  bed: `I am a quick learner and am randomly thinking about how things work.. This is my bed, where I sleep. Great ideas comes when I'm lying on my bed. When an idea strikes, I often have to write it down or else I won't be able to sleep because my mental energy is consumed by it.`,
+  resume: `This is my desk and on it is my resume. <a href="https://github.com/Ritwik740/resume/blob/08687a3d0610baafcb93c9351972c38415adb574/ritwik_mishra_resume.pdf" target="_blank">Please do check it out..</a>`,
+  projects: `I've made several projects and some of them are mentioned in the resume. Please check it out.`,
+  library: `There are a lot of programming books on my shelves.I prefer learning from books and documentations.`,
+  exit: `If you want to exit my portfolio, just close the tab. I hope you liked it..`,
+};
